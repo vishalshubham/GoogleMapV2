832a6c14ddce788d8881bf0c58ad9aa796f422a1
vishalshubham <vishal.shubham@gmail.com>
2015-09-14 23:15:25 -0400

    Markers added

--- app/src/main/java/googlemapmarker/com/googlemapmarker/MapActivity.java
+++ app/src/main/java/googlemapmarker/com/googlemapmarker/MapActivity.java
@@ -6,11 +6,34 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        final GoogleMap mMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        MarkerOptions mOptions = new MarkerOptions();
+        mOptions.title("Some place");
+        mOptions.position(new LatLng(0, 0));
+        mMap.addMarker(mOptions);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions newMarker = new MarkerOptions();
+                newMarker.position(latLng);
+                newMarker.title("New Marker");
+                mMap.addMarker(newMarker);
+            }
+        });
     }
 }
